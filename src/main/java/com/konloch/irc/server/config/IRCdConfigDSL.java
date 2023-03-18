f8a60e5ce829d446c5d593fa8d00362b1678ef96
Konloch <konloch@gmail.com>
2023-03-17 18:49:40 -0600

    DSL Rename

--- src/main/java/com/konloch/irc/server/config/IRCdConfigDSL.java
+++ src/main/java/com/konloch/irc/server/config/IRCdConfigDSL.java
@@ -6,9 +6,9 @@ import com.konloch.dsl.DSL;
  * @author Konloch
  * @since 3/3/2023
  */
-public class IRCdConfig extends DSL
+public class IRCdConfigDSL extends DSL
 {
-	public IRCdConfig()
+	public IRCdConfigDSL()
 	{
 		super('=', '%',
 				'(', ')',
