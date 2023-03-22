3cfc9a0a85b225993fd0bd350e3902e68faed792
Konloch <konloch@gmail.com>
2023-03-22 07:40:04 -0600

    Secure JOIN

--- src/main/java/com/konloch/irc/protocol/decoder/messages/impl/JOIN.java
+++ src/main/java/com/konloch/irc/protocol/decoder/messages/impl/JOIN.java
@@ -24,8 +24,9 @@ public class JOIN implements ProtocolMessage
 		if(msgVal == null || msgVal.isEmpty())
 			return;
 		
+		//don't process until they have set first nick
 		if(!user.isFlagHasSetInitialNick())
-			user.setFlagHasSetInitialNick(true);
+			return;
 		
 		for(IRCdUserListener listener : user.getIRC().getEvents().getUserEvents())
 			if(!listener.onJoinChannel(user, msgVal))
