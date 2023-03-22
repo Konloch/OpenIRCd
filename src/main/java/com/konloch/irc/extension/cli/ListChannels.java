0d75a180c0b078da1cad066962164a432ce7d51c
Konloch <konloch@gmail.com>
2023-03-21 22:10:48 -0600

    Added List Channels

--- src/main/java/com/konloch/irc/extension/cli/ListChannels.java
+++ src/main/java/com/konloch/irc/extension/cli/ListChannels.java
@@ -2,7 +2,13 @@ package com.konloch.irc.extension.cli;
 
 import com.konloch.irc.OpenIRCd;
 import com.konloch.irc.extension.Plugin;
+import com.konloch.irc.server.channel.Channel;
+import com.konloch.irc.server.client.data.UserData;
 import com.konloch.irc.server.util.cli.Command;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author Konloch
@@ -16,7 +22,24 @@ public class ListChannels implements Plugin
 		//help command
 		irc.getCLI().register(new Command("list", "List all channels", command->
 		{
-		
+			
+			ArrayList<Channel> channels = new ArrayList<>(irc.getDB().getChannels().values());
+			
+			channels.sort(Comparator.comparingInt(o -> o.getUsers().size()));
+			
+			for(Channel channel : channels)
+			{
+				StringBuilder sb = new StringBuilder("\t+ ");
+				
+				if(channel.isPermanent())
+					sb.append("[PERMANENT] ");
+				
+				sb.append(channel.getName()).append(" - ");
+				sb.append(channel.getUsers().size()).append(" connected - ");
+				sb.append(Arrays.toString(channel.getModes().toArray()));
+				
+				System.out.println(sb);
+			}
 		}));
 	}
 }
