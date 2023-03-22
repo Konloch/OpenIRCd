09ba628aae36832bf3988755feb46ccaa80092bc
Konloch <konloch@gmail.com>
2023-03-22 06:15:59 -0600

    Extend List Users

--- src/main/java/com/konloch/irc/extension/cli/ListUsers.java
+++ src/main/java/com/konloch/irc/extension/cli/ListUsers.java
@@ -2,14 +2,13 @@ package com.konloch.irc.extension.cli;
 
 import com.konloch.irc.OpenIRCd;
 import com.konloch.irc.extension.Plugin;
+import com.konloch.irc.server.channel.Channel;
 import com.konloch.irc.server.client.User;
 import com.konloch.irc.server.client.data.PermissionUserGroup;
 import com.konloch.irc.server.client.data.UserData;
 import com.konloch.irc.server.util.cli.Command;
-import com.konloch.irc.server.util.cli.CommandArgument;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 
 /**
  * @author Konloch
@@ -20,7 +19,7 @@ public class ListUsers implements Plugin
 	@Override
 	public void install(OpenIRCd irc)
 	{
-		//help command
+		//list users command
 		irc.getCLI().register(new Command("who", "List all users - connected or registered", command->
 		{
 			if(command.getArguments().length == 0)
@@ -67,6 +66,32 @@ public class ListUsers implements Plugin
 			}
 			else
 			{
+				String channelName = command.asString(0);
+				Channel channel = irc.getChannels().get(channelName);
+				
+				if(channel == null)
+				{
+					System.out.println("`" + channelName + "` is closed.");
+					return;
+				}
+				
+				System.out.println("Connected Users:");
+				if(irc.getConnected().isEmpty())
+					System.out.println("\tNone");
+				else for(User connectedUser : channel.getUsers())
+				{
+					StringBuilder sb = new StringBuilder("\t+ ");
+					
+					if(!connectedUser.isFlagHasAuthorizedNick() && irc.getDB().getRegisteredUsers().containsKey(connectedUser.getNick()))
+						sb.append("[UNAUTHORIZED] ");
+					
+					sb.append(connectedUser.getNick()).append(" - ");
+					sb.append(connectedUser.getUser()).append(" - ");
+					sb.append(connectedUser.getRealName());
+					
+					System.out.println(sb);
+				}
+				
 				//TODO list all users connected to the channel, then all users with flags for this channel
 			}
 		}).addOptionalArgument("channel", "Any channel to list the users from").get());
